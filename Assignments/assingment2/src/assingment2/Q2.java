b8dbfa7c7ea7c7ba5646a1d9290d41262d0356c5
UttkarshRithe <yashrithe38@gmail.com>
2026-02-24 23:37:49 +0530

    Upload assignment2

---
+++ Assignments/assingment2/src/assingment2/Q2.java
@@ -0,0 +1,33 @@
+package assingment2;
+class Shape 
+{
+	double  area;
+	void calculateArea(int a,int b)
+	{
+		int base=a;
+		int height=b;
+		double area=(1/2)*base*height;
+		System.out.println("Area of triangle:"+area);
+	}
+}
+class Triangle 
+{
+	int base,height;
+}
+class Rectangle
+{
+	int breadth,length;
+}
+class Circle
+{
+	int radius;
+}
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
